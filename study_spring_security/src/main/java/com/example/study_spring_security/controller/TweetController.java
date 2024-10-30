5a4cc06370e2ac1bed730ddc70178b7915126ee3
Vinicius Alves <103587422+Viniciusalves16@users.noreply.github.com>
2024-10-29 21:15:44 -0300

    Ajustado validação que concede ao admin acesso a excluir qualquer tweet ✅

--- study_spring_security/src/main/java/com/example/study_spring_security/controller/TweetController.java
+++ study_spring_security/src/main/java/com/example/study_spring_security/controller/TweetController.java
@@ -1,15 +1,16 @@
 package com.example.study_spring_security.controller;
 
 import com.example.study_spring_security.dto.CreateTweetDto;
+import com.example.study_spring_security.entities.Role;
 import com.example.study_spring_security.entities.Tweet;
 import com.example.study_spring_security.repository.TweetRepository;
 import com.example.study_spring_security.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -28,14 +29,35 @@ public class TweetController {
     @Transactional
     public ResponseEntity<Void> createTweet(@RequestBody CreateTweetDto createTweetDto,
                                             JwtAuthenticationToken token) { // permite identificar qual usuário esta vindo na requisição
-    var user = userRepository.findById(UUID.fromString(token.getName()));
-    var tweet = new Tweet();
-    tweet.setUser(user.get());
-    tweet.setContent(createTweetDto.content());
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        var tweet = new Tweet();
+        tweet.setUser(user.get());
+        tweet.setContent(createTweetDto.content());
 
-    tweetRepository.save(tweet);
-    return ResponseEntity.ok().build();
+        tweetRepository.save(tweet);
+        return ResponseEntity.ok().build();
 
+    }
+
+    @DeleteMapping("tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,
+                                            JwtAuthenticationToken token) {
+
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        // Busca o tweet no banco de dados
+        Tweet tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var isAdmin = user.get().getRole().stream().anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+        // Verifica se o usuário que está tentando deletar o tweet é o mesmo que o criou
+        if (isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))){
+          tweetRepository.deleteById(tweetId);
+        }else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        return ResponseEntity.ok().build();
     }
 
 }
